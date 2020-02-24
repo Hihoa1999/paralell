a217c67a727c76024f7f84a40360f47353c36731
Nguyen Chi Hoa <58041946+Hihoa1999@users.noreply.github.com>
2020-02-24 20:28:09 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="sty.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div class="pic1 hide">
+		<div class="text1">
+			<span class="bor tran">
+		    NGUYEN CHI HOA
+		  </span>
+		
+		</div>
+	</div>
+
+	<section class="section section-light">
+		<h2>Alexander Spatari</h2>
+		<p>Im selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you cant handle me at my worst, then you sure as hell dont deserve me at my best.
+			<br>Marilyn Monroe</p>
+	</section>
+
+	<div class="pic2">
+		<div class="text1">
+			<span class="bor tran">
+		    NEW YORK
+		  </span>
+		
+
+		</div>
+	</div>
+
+	<section class="section section-dark">
+		<h2>New york</h2>
+		<p>Youve gotta dance like there’s nobody watching,Love like youll never be hurt,Sing like theres nobody listening,And live like its heaven on earth. <br>William W. Purkey</p>
+	</section>
+
+	<div class="pic3">
+		<div class="text1">
+			<span class="bor tran">
+		    TOKYO
+		  </span>
+		
+		</div>
+	</div>
+
+	<section class="section section-dream ">
+		<h2>Tokyo</h2>
+		<p>There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle. <br> Albert Einstein</p>
+	</section>
+</body>
+</html>
